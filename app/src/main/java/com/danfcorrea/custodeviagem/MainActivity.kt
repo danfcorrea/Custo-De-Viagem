e15d2bbd0b41bf9ce33f94d6364a8e3a95339dba
Daniel Corrêa <daniel.1997.df@gmail.com>
2024-01-22 15:00:48 -0300

    feat: criacao de funcoes basicas para funcionamento do app

--- app/src/main/java/com/danfcorrea/custodeviagem/MainActivity.kt
+++ app/src/main/java/com/danfcorrea/custodeviagem/MainActivity.kt
@@ -2,14 +2,50 @@ package com.danfcorrea.custodeviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.widget.Button
+import com.google.android.material.textfield.TextInputLayout
+import com.google.android.material.textview.MaterialTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private var distanceValue: Double = 0.0
+    private var priceValue: Double = 0.0
+    private var consumeValue: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        val calcButton = findViewById<Button>(R.id.calc)
+        calcButton.setOnClickListener { click() }
     }
+
+    private fun click() {
+        loadValues()
+        returnValues(
+            findViewById(R.id.result),
+            String.format("%.2f", calculate(distanceValue, priceValue, consumeValue))
+        )
+    }
+
+    private fun calculate(distance: Double, price: Double, consume: Double): Double {
+        return distance / consume * price
+    }
+
+    private fun loadValues() {
+        distanceValue = findViewById<TextInputLayout>(R.id.distance)
+            .editText?.text.toString().replace(',', '.').toDouble()
+        priceValue = findViewById<TextInputLayout>(R.id.price)
+            .editText?.text.toString().replace(',', '.').toDouble()
+        consumeValue = findViewById<TextInputLayout>(R.id.consumption)
+            .editText?.text.toString().replace(',', '.').toDouble()
+    }
+
+    private fun returnValues(view: MaterialTextView, valor: String) {
+        view.text = buildString {
+            append(valor)
+            append(" R$")
+        }
+    }
+
 }
